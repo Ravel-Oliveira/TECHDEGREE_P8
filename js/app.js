4d170e36ce8b28070b186f59330fdf5679db2f91
Ravel-Oliveira <ravel.sbodg@gmail.com>
2024-11-18 17:11:34 -0300

    Search
    JS search function added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const url = 'https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture,nat';
 const section = document.getElementById('content');
+const search = document.getElementById('searchBox');
 let employeeData;
 
 
@@ -8,6 +9,7 @@ function displayEmployees(data) {
         const card = document.createElement('div');
         card.className = 'employee-box';
         card.setAttribute('data-index', index);
+        card.setAttribute('data-name', `${employee.name.first} ${employee.name.last}`);
         section.appendChild(card);
         card.innerHTML = `
         <img src=${employee.picture.large} alt=${employee.name.first} profile picture>
@@ -30,5 +32,27 @@ function fetchEmployee(url) {
             displayEmployees(employeeData);
         })
 };
-
 fetchEmployee(url);
+
+
+
+// Search function
+
+function searchBox() {
+    let searchValue = search.value.toUpperCase();
+    let employees = document.getElementsByClassName('employee-box');
+    let boxData
+
+    for (let i = 0; i < employees.length; i++) {
+        boxData = employees[i].getAttribute('data-name')
+        if (boxData.toUpperCase().indexOf(searchValue) > -1) {
+            employees[i].style.display = "";
+        } else {
+            employees[i].style.display = "none";
+        }
+    }
+}
+
+search.addEventListener('input', () => {
+    searchBox()
+});
